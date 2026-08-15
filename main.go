b668e6c5347e21042c4061dc639234ff0a499dba
Christopher Hernandez <psychosisjr@gmail.com>
2026-08-14 22:35:07 -0400

    must change control flow inside of the handlerRegister

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
-	"github.com/fanatcx/gator/internal/config"
 	"os"
+	"time"
+
+	"github.com/fanatcx/gator/internal/config"
+	"github.com/fanatcx/gator/internal/database"
+	"github.com/google/uuid"
+
+	_ "github.com/lib/pq"
 )
+
+// We are importing for side effects, not really using it
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 type command struct {
@@ -28,6 +39,38 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("User has been set sucessfully.")
+	return nil
+}
+
+// needs to check for username (arg 2)
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("username not supplied as an argument after command.")
+	}
+	// empty user slot
+	u, err := s.db.CreateUser(context.Background(), database.CreateUserParams{})
+	if err != nil {
+		return err
+	}
+	// user already exists? is that what err is? confused. Also, context? Do I pass in background or point to a parent. Not explained in lesson
+	_, err = s.db.GetUser(context.Background(), u.Name) 
+	if err != nil {
+
+		return errors.New("user already exists.")
+	}
+	// user is new
+	u.ID = uuid.New()
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+	u.Name = cmd.arguments[0]
+	s.cfg.CurrentUserName = u.Name
+
+	fmt.Printf("user: '%v' created successfully at %v and updated at %v. Current uuid: %v", 
+	u.Name, 
+	u.CreatedAt, 
+	u.UpdatedAt, 
+	u.ID)
+	
 	return nil
 }
 
@@ -52,34 +95,47 @@ func (c *commands) register(name string, f func(*state, command) error) {
 	c.command[name] = f // assign to map
 }
 
-func main() {
 
+func main() {
 	if len(os.Args) < 2 {
 		fmt.Print("Error, not enough arguments. Please type a command.")
 		os.Exit(1)
 	}
 
 	var stateST state
+
+	// read from json on disk. returns config object or error
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Printf("%v", err)
 		os.Exit(1)
 	}
 	stateST.cfg = &cfg
+	db, err := sql.Open("postgres", stateST.cfg.DbURL)
 
+	// A pointer to queries?
+	dbQueries := database.New(db)
+	stateST.db = dbQueries
+	
+	// create the commands
 	cmds := commands{
 		command: make(map[string]func(*state, command) error),
 	}
+	// handler
 	cmds.register("login", handlerLogin)
-
+	cmds.register("register", handlerRegister)
 	cmd := command{
 		name:      os.Args[1],  // command name
-		arguments: os.Args[2:], // rest of the arguments SHOULD be
+		arguments: os.Args[2:], // rest of the arguments, if any
 	}
 
+	// self note: returns an error if failed to run a command through a handler
 	if err := cmds.run(&stateST, cmd); err != nil {
 		fmt.Printf("%v", err)
 		os.Exit(1)
 	}
+
+
+	
 
 }
