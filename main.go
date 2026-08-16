c9e84c77b689d86a42ca8bb91a4a3db5a216c39a
Christopher Hernandez <psychosisjr@gmail.com>
2026-08-15 22:04:03 -0400

    working code, need to create proper migrations

--- main.go
+++ main.go
@@ -33,12 +33,16 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.arguments) < 1 {
 		return errors.New("the login handler expects a username argument.")
 	}
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err != nil {
+		return errors.New("user does not exist.")
+	}
 
-	if err := s.cfg.SetUser(cmd.arguments[0]); err != nil {
+	if err = s.cfg.SetUser(cmd.arguments[0]); err != nil {
 		return fmt.Errorf("Could not set user: %w", err)
 	}
 
-	fmt.Println("User has been set sucessfully.")
+	fmt.Println("User is logged in sucessfully.")
 	return nil
 }
 
@@ -47,31 +51,37 @@ func handlerRegister(s *state, cmd command) error {
 	if len(cmd.arguments) == 0 {
 		return errors.New("username not supplied as an argument after command.")
 	}
-	// empty user slot
-	u, err := s.db.CreateUser(context.Background(), database.CreateUserParams{})
-	if err != nil {
-		return err
-	}
-	// user already exists? is that what err is? confused. Also, context? Do I pass in background or point to a parent. Not explained in lesson
-	_, err = s.db.GetUser(context.Background(), u.Name) 
-	if err != nil {
 
-		return errors.New("user already exists.")
-	}
-	// user is new
-	u.ID = uuid.New()
-	u.CreatedAt = time.Now()
-	u.UpdatedAt = time.Now()
-	u.Name = cmd.arguments[0]
-	s.cfg.CurrentUserName = u.Name
+	// check if user exists ->
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
 
-	fmt.Printf("user: '%v' created successfully at %v and updated at %v. Current uuid: %v", 
-	u.Name, 
-	u.CreatedAt, 
-	u.UpdatedAt, 
-	u.ID)
-	
-	return nil
+	// not found, new user needed
+	if err != nil {
+		// create user
+		u, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      cmd.arguments[0],
+		})
+		if err != nil {
+			return err
+		}
+		// set config locally
+		if err = s.cfg.SetUser(u.Name); err != nil {
+			return err
+		}
+
+		fmt.Printf("user: '%v' created successfully at %v and updated at %v. Current uuid: %v",
+			u.Name,
+			u.CreatedAt,
+			u.UpdatedAt,
+			u.ID)
+
+		return nil
+	}
+
+	return errors.New("user already exists")
 }
 
 type commands struct {
@@ -80,14 +90,15 @@ type commands struct {
 
 func (c *commands) run(s *state, cmd command) error {
 	valFunc, exists := c.command[cmd.name]
+
 	if !exists {
 		return errors.New("failed to run command, command does not exist.")
 	}
-
 	// run command, return err if not successful
 	if err := valFunc(s, cmd); err != nil {
-		return fmt.Errorf("error running command %s: %w", cmd.name, err)
+		return fmt.Errorf("error running command '%s': %w", cmd.name, err)
 	}
+
 	return nil
 }
 
@@ -95,10 +106,9 @@ func (c *commands) register(name string, f func(*state, command) error) {
 	c.command[name] = f // assign to map
 }
 
-
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Print("Error, not enough arguments. Please type a command.")
+		fmt.Println("Error, not enough arguments. Please type a command.")
 		os.Exit(1)
 	}
 
@@ -116,7 +126,7 @@ func main() {
 	// A pointer to queries?
 	dbQueries := database.New(db)
 	stateST.db = dbQueries
-	
+
 	// create the commands
 	cmds := commands{
 		command: make(map[string]func(*state, command) error),
@@ -131,11 +141,7 @@ func main() {
 
 	// self note: returns an error if failed to run a command through a handler
 	if err := cmds.run(&stateST, cmd); err != nil {
-		fmt.Printf("%v", err)
+		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
-
-
-	
-
 }
